b7631c74c5443d28dea9675d181fba76411c4831
ssani7 <sanaullah.sani756@gmail.com>
2023-02-22 23:45:53 +0600

    try catch

--- index.js
+++ index.js
@@ -20,26 +20,37 @@ const io = require("socket.io")(3001, {
 })
 
 io.on("connection", socket => {
-
     socket.on("get-document", async documentID => {
-        const document = await findOrCreateDocument(documentID)
-        socket.join(documentID)
-        socket.emit("load-document", document.data)
+        try {
+            const document = await findOrCreateDocument(documentID)
+            socket.join(documentID)
+            socket.emit("load-document", document.data)
 
-        socket.on("send-changes", (delta) => {
-            socket.broadcast.to(documentID).emit("recieve-changes", delta)
-        })
+            socket.on("send-changes", (delta) => {
+                socket.broadcast.to(documentID).emit("recieve-changes", delta)
+            })
 
-        socket.on("save-document", async data => {
-            await Document.findByIdAndUpdate(documentID, { data })
-        })
+            socket.on("save-document", async data => {
+                await Document.findByIdAndUpdate(documentID, { data })
+            })
+        } catch (error) {
+
+        }
+
     })
+
+
 })
 
 async function findOrCreateDocument(id) {
-    if (!id) return
+    try {
+        if (!id) return
 
-    const document = await Document.findById(id);
-    if (document) return document
-    return await Document.create({ _id: id, data: "" })
+        const document = await Document.findById(id);
+        if (document) return document
+        return await Document.create({ _id: id, data: "" })
+    } catch (error) {
+        return
+    }
+
 }
